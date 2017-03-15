ca4b1aabf68e23b6f3845bd7091b82f5feb6304d
Mike Raineri <michael.raineri@dell.com>
2017-03-15 15:33:01 -0400

    Fixed file reference

--- test/odata_check.sh
+++ test/odata_check.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /tmp/Redfish-Tools/odata-csdl-validator/odata_validator.py ../Test_Schema.xml
+python3 /tmp/Redfish-Tools/odata-csdl-validator/odata_validator.py Test_Schema.xml
